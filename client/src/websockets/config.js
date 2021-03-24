c1fbf7bb70f66da32062800bf40aadaba553dc82
Roman Kurychyk <beq.dev@gmail.com>
2021-03-25 00:07:57 +0200

    support secured websocket

--- client/src/websockets/config.js
+++ client/src/websockets/config.js
@@ -1,1 +1,1 @@
-export const webSocketUrl = "ws://"+window.location.host+"/ws";
+export const webSocketUrl = "ws"+(window.location.protocol.substr(0,5)==="https"?"s":"")+"://"+window.location.host+"/ws";
