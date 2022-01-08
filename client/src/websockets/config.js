34b0039587dd8f1db770425da6ac553354950f65
ksingh7 <karan.singh731987@gmail.com>
2022-01-08 17:16:44 +0530

    fixes to chat backend and frotend apps for openshift

--- client/src/websockets/config.js
+++ client/src/websockets/config.js
@@ -1,1 +1,1 @@
-export const webSocketUrl = "ws"+(window.location.protocol.substr(0,5)==="https"?"s":"")+"://"+ process.env.REACT_CHAT_APP_BACKEND +"/ws";
+export const webSocketUrl = "ws"+(window.location.protocol.substr(0,5)==="https"?"s":"")+"://"+ process.env.REACT_APP_CHAT_BACKEND +"/ws";
