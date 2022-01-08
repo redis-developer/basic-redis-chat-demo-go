aa8302b39fc82524ea5c6fb19d67390f2b871044
ksingh7 <karan.singh731987@gmail.com>
2022-01-08 21:58:26 +0530

    fixes to chat backend and frotend apps for openshift

--- client/src/websockets/config.js
+++ client/src/websockets/config.js
@@ -1,12 +1,12 @@
 export const webSocketUrl = "ws"+(window.location.protocol.substr(0,5)==="https"?"s":"")+"://"+ process.env.REACT_APP_CHAT_BACKEND +"/ws";
 //export const webSocketUrl = "wss://"+ process.env.REACT_APP_CHAT_BACKEND +"/ws";
-export const HTTP_PROXY = REACT_APP_HTTP_PROXY
-export const CLIENT_LOCATION = REACT_APP_CLIENT_LOCATION
-export const SERVER_ADDRESS = REACT_APP_SERVER_ADDRESS
+export const HTTP_PROXY = process.env.REACT_APP_HTTP_PROXY
+export const CLIENT_LOCATION = process.env.REACT_APP_CLIENT_LOCATION
+export const SERVER_ADDRESS = process.env.REACT_APP_SERVER_ADDRESS
 
 console.log("value of REACT_APP_CHAT_BACKEND is : " + process.env.REACT_APP_CHAT_BACKEND);
-console.log("value of HTTP_PROXY is : " + process.env.HTTP_PROXY);
 console.log("value of webSocketUrl is : " + webSocketUrl);
 console.log("value of window.location.protocol is : " + window.location.protocol);
-console.log("value of CLIENT_LOCATION is : " + process.env.CLIENT_LOCATION);
-console.log("value of SERVER_ADDRESS is : " + process.env.SERVER_ADDRESS);
+console.log("value of HTTP_PROXY is : " + HTTP_PROXY);
+console.log("value of CLIENT_LOCATION is : " + CLIENT_LOCATION);
+console.log("value of SERVER_ADDRESS is : " + SERVER_ADDRESS);
